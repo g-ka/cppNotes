98e52a0c2e59dfeaebaa7b26609e2ca258fdc9b6
g-ka <gkaphilathithiya@gmail.com>
2024-01-18 17:46:59 +0530

    kadanes_complete

--- striver's_A2Z_DSA/03) arrays/3.2) arrays_medium/3.2.4,5) max_sum_subarray_kadanes/max_sum_subarray_kadanes.cpp
+++ striver's_A2Z_DSA/03) arrays/3.2) arrays_medium/3.2.4,5) max_sum_subarray_kadanes/max_sum_subarray_kadanes.cpp
@@ -19,12 +19,22 @@ int maxSubarraySumOf(int a[], const int SIZE){
   
   int maxSum = INT_MIN;
   int tempSum = 0;
+  int tempStart = -1;
+  int start = -1;
+  int end = -1;
 
   for(int i=0; i<SIZE; i++){
+    if(tempSum == 0) tempStart = i;
     tempSum += a[i];
-    if(tempSum > maxSum) maxSum = tempSum;
+    if(tempSum > maxSum){
+      maxSum = tempSum;
+      start = tempStart;
+      end = i;
+    }
     if(tempSum < 0) tempSum = 0;
   }
+
+  for(int i=start; i<=end; i++) std::cout << a[i] << " ";
 
   return (maxSum == INT_MIN ? 0 : maxSum);
 
