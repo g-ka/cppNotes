f6f17f412508c0b921279f696aba5f192236e773
g-ka <gkaphilathithiya@gmail.com>
2024-01-18 13:02:06 +0530

    3.2.6

--- striver's_A2Z_DSA/03) arrays/3.2) arrays_medium/3.2.6) stock_buy_sell/stock_buy_sell.cpp
+++ striver's_A2Z_DSA/03) arrays/3.2) arrays_medium/3.2.6) stock_buy_sell/stock_buy_sell.cpp
@@ -14,4 +14,21 @@ int main(){
 
 void predict(int a[], const int SIZE){
 
+  int buy = -1;
+  int sell = -1;
+  int maxProfit = 0;
+
+  for(int i=0; i<SIZE-1; i++){
+    for(int j=i+1; j<SIZE; j++){
+      if(a[j] - a[i] > maxProfit){
+        buy = i;
+        sell = j;
+        maxProfit = a[j] - a[i];
+      }
+    }
+  }
+
+  if(maxProfit) std::cout << "Buy on day " << buy+1 << " and sell on day " << sell+1 << " for a profit of " << maxProfit << " units";
+  else std::cout << "No profit could be made";
+
 }
