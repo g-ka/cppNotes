0f1df684566e9a49feeba2d7f12182d2f02be775
g-ka <gkaphilathithiya@gmail.com>
2024-01-20 22:45:40 +0530

    3sum_optimal

--- striver's_A2Z_DSA/03) arrays/3.3) arrays_hard/3.3.2) 3Sum_optimal/3Sum_optimal.cpp
+++ striver's_A2Z_DSA/03) arrays/3.3) arrays_hard/3.3.2) 3Sum_optimal/3Sum_optimal.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
 
-bool TwoSumOf(int a[], int, int);
+void tripletsIn(int a[], const int SIZE, const int K);
 void sort(int a[], int low, int high);
 void merge(int a[], int low, int mid, int high);
 
 int main(){
 
-  int numbers[] = {-1,0,1,1,-1,2,1,0,1,-1,0};
+  int numbers[] = {-1,0,1,2,-1,-4};
   const int SIZE = sizeof(numbers)/sizeof(int);  
-  bool isDoublets;
 
-  isDoublets = TwoSumOf(numbers, SIZE, 2);
-
-  std::cout << (isDoublets ? "YES" : "NO");
+  tripletsIn(numbers, SIZE, 0);
 
   return 0;
 }
 
-bool TwoSumOf(int a[], const int SIZE, const int K){
-  
-  // sorting:
+void tripletsIn(int a[], const int SIZE, const int K){
+
+  // sort:
   sort(a, 0, SIZE-1);
 
-  // 2-pointer approach:
-  int start = 0;
-  int end = SIZE-1;
+  // 2sum + pointer:  
+  for(int i=0; i<SIZE; i++){    
+    int start = i+1;
+    int end = SIZE-1;    
+    while(start<end){    
 
-  while(start<end){
-    if(a[start] + a[end] < K) start++;
-    else if(a[start] + a[end] > K) end--;
-    else return true;
+      if(a[start]+a[end]+a[i] > K) end--;
+      else if(a[start]+a[end]+a[i] < K) start++;
+      else{        
+        std::cout << "[ " << start << ", " << end << ", " << i << " ]\n";
+        start++;
+      }
+    }
   }
-
-  return false;
 
 }
 
@@ -40,7 +40,7 @@ bool TwoSumOf(int a[], const int SIZE, const int K){
 
 void sort(int a[], int low, int high){
   if(low == high) return;
-  int mid = (low+high)/2;
+  int mid = (high+low)/2;
   sort(a, low, mid);
   sort(a, mid+1, high);
   merge(a, low, mid, high);
@@ -48,7 +48,7 @@ void sort(int a[], int low, int high){
 
 void merge(int a[], int low, int mid, int high){
   int p1 = low;
-  int p2 = mid+1;
+  int p2 = mid + 1;
   int temp[high-low+1];
   int index = 0;
 
@@ -75,9 +75,9 @@ void merge(int a[], int low, int mid, int high){
     index++;
   }
 
-  index = 0 ;
+  index = 0;
   for(int i=low; i<=high; i++){
     a[i] = temp[index];
     index++;
-  }
+ }
 }
