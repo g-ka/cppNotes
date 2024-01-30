d6e2f2b5b90598301a60d7bc645c6874fb514498
g-ka <gkaphilathithiya@gmail.com>
2024-01-30 13:44:09 +0530

    3.3.10_brute

---
+++ striver's_A2Z_DSA/03) arrays/3.3) arrays_hard/3.3.10) inversions/inversions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+int inversionNum(std::vector<int> v);
+
+int main(){
+
+  std::vector<int> v{5,4,3,2,1};
+  int count;
+
+  count = inversionNum(v);
+
+  std::cout << count;
+
+  return 0;
+}
+
+int inversionNum(std::vector<int> v){
+  int count = 0;
+  for(int i=0; i<v.size(); i++){
+    for(int j=i+1; j<v.size(); j++){
+      if(v[j] < v[i]) count++;
+    }
+  }
+  return count;
+}
