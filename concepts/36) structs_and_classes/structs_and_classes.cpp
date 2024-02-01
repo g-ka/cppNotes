2368b4bc54f48c61f94fea907b5ceec3ed763f83
g-ka <gkaphilathithiya@gmail.com>
2024-02-01 12:20:55 +0530

    structs and classes

---
+++ concepts/36) structs_and_classes/structs_and_classes.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+// Three types of access modifiers:
+// 1) public: can be accessed in any class of the code
+// 2) private: can be accessed only inside the class where it is defined
+// 3) protected: same as private except that it can be used inside its subclass
+
+// Two ways to create member functions:
+// 1) inside class
+// 2) outside class
+
+// -- constructors and destructors are two special type of member functions
+// -- The constructor will be called everytime a new instance of the class is created
+// -- The destructor is called everytime an object reaches its end of the scope, thus
+// deleted
+
+// -- key difference between structures and classes:
+// 1) default access modifier of structs are public while for classes it is private
+// 2) structs doesn't support inheritance, polymorphism and abstraciton while classes does it
+// 3) NULL values are not possible in structs but possible in classes 
+// 4) structs are stored in stack memory while classes are stored in heap memory
+// 5) keyword 'struct' is used to create structs while 'class' is used for classes
+// -- similarities between structures and classes:
+// 1) both support encapsulation
+// 2) both have data members and member functions
+// 3) both have constructors and destructors
+
+class car{
+  // access modifier:
+  public:
+
+  // data memebers:
+  std::string name;
+  std::string model;
+  bool isReg = NULL;
+
+  // member functions:
+  void setName(std::string carName){
+    name = carName;
+  }
+  void setModel(std::string carModel){
+    model = carModel;
+  }
+  void setReg(bool carIsReg);
+  void displayDetails(){
+    std::cout << name << " " << model << " " << isReg << "\n";
+  }
+
+  // constructors:
+  car(){
+    return;
+  }
+  car(std::string carName){
+    name = carName;
+  }
+  car(std::string carName, std::string carModel){
+    name = carName;
+    model = carModel;
+  }
+
+  // destructors:
+  ~car(){
+    std::cout << "Destructor\n";
+  }
+};
+
+// defining a member function outside the class
+void car::setReg(bool carIsReg){
+  isReg = carIsReg;
+}
+
+int main(){
+
+  // creating an object without passing arguments to constructors:
+  car chevy;
+  chevy.setName("Chevrolet");
+  chevy.setModel("Camaro");
+  chevy.setReg(true);
+  
+  chevy.displayDetails();
+
+  // creating an object by passing arguments to constructors:
+  car dodger("Dodger", "Challenger");
+  dodger.setReg(false);
+
+  dodger.displayDetails();
+  
+  return 0;
+}
