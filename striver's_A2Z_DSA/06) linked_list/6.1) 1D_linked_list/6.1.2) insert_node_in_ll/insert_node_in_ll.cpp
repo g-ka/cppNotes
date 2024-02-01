62bb418b2030f249cfb0036895e098d0774d7ac8
g-ka <gkaphilathithiya@gmail.com>
2024-02-01 19:16:47 +0530

    node_insertion

--- striver's_A2Z_DSA/06) linked_list/6.1) 1D_linked_list/6.1.2) insert_node_in_ll/insert_node_in_ll.cpp
+++ striver's_A2Z_DSA/06) linked_list/6.1) 1D_linked_list/6.1.2) insert_node_in_ll/insert_node_in_ll.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 
+class Node{
+  public:
+  int data;
+  Node* next;
+  Node(){
+    data = 0;
+    next = nullptr;
+  }
+  Node(int data1){
+    data = data1;
+    next = nullptr;
+  }
+  Node(int data1, Node* next1){
+    data = data1;
+    next = next1;
+  }
+};
+
+Node* insertAtFirst(int value, Node* head){
+  Node* currentNode = new Node(value);
+  currentNode->next = head;
+  head = currentNode;  
+  return head;
+}
+
+void printLL(Node* head){
+  for(Node* pointer=head; pointer!=nullptr; pointer=pointer->next){
+    std::cout << pointer->data << " ";
+  }
+}
+
 int main(){
+
+  Node* head = nullptr;
+  head = insertAtFirst(40, head);
+  head = insertAtFirst(30, head);
+  head = insertAtFirst(20, head);
+  head = insertAtFirst(10, head);
+
+  printLL(head);
+
   return 0;
 }
