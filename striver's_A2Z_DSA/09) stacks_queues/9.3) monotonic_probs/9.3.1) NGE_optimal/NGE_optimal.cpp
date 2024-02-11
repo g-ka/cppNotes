542899660658ba22bfa4512f21fa7b07e9cd9ac6
g-ka <gkaphilathithiya@gmail.com>
2024-02-11 10:35:49 +0530

    13.1.1

---
+++ striver's_A2Z_DSA/09) stacks_queues/9.3) monotonic_probs/9.3.1) NGE_optimal/NGE_optimal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+std::vector<int> NGE(std::vector<int> v);
+
+int main(){
+
+  std::vector<int> v{5,7,1,7,6,0};
+
+  std::vector<int> result = NGE(v);
+
+  for(auto i: result) std::cout << i << " ";
+
+  return 0;
+}
+
+std::vector<int> NGE(std::vector<int> v){
+  
+}
---
+++ striver's_A2Z_DSA/13) Binary_trees/13.1) easy_probs/13.1.1) intro_theory/intro_theory.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// Binary tree:
+// A hierarchical data structure where each node has at most 2 child nodes
+
+// Different types of binary tree:
+// 1) Full-binary tree:
+// -- All nodes should have either 0 or 2 child nodes
+// 2) Complete-binary tree:
+// -- All the levels must have complete set of nodes
+// -- The last level however could be incomplete with each node at its left most position
+// 3) Perfect-binary tree:
+// -- All lead nodes are at the same level
+// 4) Balanced tree:
+// -- Height of the tree at max should be log(N) where N is the number of nodes
+// 5) Degenerate tree:
+// -- Every node has only one child element essentially forming a single line of tree, similar to linked-list
+
+int main(){
+  return 0;
+}
