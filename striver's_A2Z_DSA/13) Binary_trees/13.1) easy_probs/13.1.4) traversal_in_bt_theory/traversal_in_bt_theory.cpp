c4c00460d3d52ddef59fb35db43885c30f8e5b38
g-ka <gkaphilathithiya@gmail.com>
2024-02-11 11:12:36 +0530

    13.1.4

---
+++ striver's_A2Z_DSA/13) Binary_trees/13.1) easy_probs/13.1.4) traversal_in_bt_theory/traversal_in_bt_theory.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// There are 4 ways to traverse in a binary tree:
+// DFS:
+// 1) Pre-order - root left right
+// 2) Inorder - left root right
+// 3) Post-order - left right root
+// In above methods, for every sub tree you encounter, apply its principle for it
+// BFS:
+// 4) Traverse by each level by printing every node from it from left to right
+
+int main(){
+  return 0;
+}
